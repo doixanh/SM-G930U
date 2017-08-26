a4a25662cc3c5d6cbcdb67d1dc48ae1ffb958ad4
doixanh <doixanh@yahoo.com>
2017-08-27 00:10:58 +0700

    Set perm for /su. Reduce log verbosity

--- Install-TinySU/launch.tinysu.sh
+++ Install-TinySU/launch.tinysu.sh
@@ -1,25 +1,7 @@
 #!/system/bin/sh
 
 setenforce 0
-
-me=`id`
-log -p i -t TinySUStart "I am $me"
-
-log -p i -t TinySUStart "remounting /"
 mount -o rw,remount rootfs /
-me=$?
-log -p i -t TinySUStart "mount returned $me"
-
-log -p i -t TinySUStart "creating /su"
 mkdir /su
-me=$?
-log -p i -t TinySUStart "mkdir returned $me"
-
-ls -la | while read -r line; do
-        log -p i -t TinySUStart "$line"
-done
-
-mount | grep rootfs | while read -r line; do
-        log -p i -t TinySUStart "Mounted $line"
-done
-
+chmod 777 /su
+su -d &
